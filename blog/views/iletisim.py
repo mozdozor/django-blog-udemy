e313b0f35e2b5378faa0abab4ea0ee8adc3f51dc
Muhammed AYDOĞAN <muhammetay651@gmail.com>
2021-03-08 15:07:25 +0300

    kodlar optimize edildi

---
+++ blog/views/iletisim.py
@@ -0,0 +1,8 @@
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def iletisim(request):
+    return HttpResponse("<h1>Merhaba</h1>")
+
--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import iletisim
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("iletisim",iletisim)
 ]
