dcca829628284fa6f39689f41fca6bb6f6faac80
Muhammed AYDOĞAN <muhammetay651@gmail.com>
2021-03-09 12:58:34 +0300

    fsf

--- blog/views/iletisim.py
+++ blog/views/iletisim.py
@@ -4,9 +4,11 @@ from django.http import HttpResponse
 
 
 def iletisim(request):
-   
+    context={
+        "key" : "başlık içerik naber"
+    }
     #return HttpResponse("<h1>Merhaba</h1>")
-    return render(request,"pages/iletisim.html",context={})
+    return render(request,"pages/iletisim.html",context=context)
 
 
 #template/pages/aanasayfa.html yazmadık çünkü django ilk olarak template klasörü varmı diye balıyor eğer varsa
