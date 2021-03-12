0debdab9df9a241fd4fd42037ab026c76078a7db
Muhammed AYDOĞAN <muhammetay651@gmail.com>
2021-03-12 14:10:58 +0300

    şifre değiştirme yapıldı

---
+++ account/views/__init__.py
@@ -0,0 +1,2 @@
+from .cikis import cikis
+from .sifre_degistirme import sifre_degistir
--- blog/views/yorum_sil.py
+++ blog/views/yorum_sil.py
@@ -1,12 +1,13 @@
 from blog.models import YorumModel
 from django.shortcuts import get_object_or_404,redirect
 from django.contrib.auth.decorators import login_required
-
+from django.contrib import messages
 
 @login_required(login_url="/")
 def yorum_sil(request,id):
     yorum=get_object_or_404(YorumModel,id=id)
     if yorum.yazan==request.user or yorum.yazı.yazar==request.user:
         yorum.delete()
+        messages.success(request,"Yorum başarıyla silindi")
         return redirect("detay",slug=yorum.yazı.slug)
     return redirect("anasayfa")  #eğer zarar verecek bir kullanici linki koyyalayıpyapıştırdıysa anasayfaya yönlendirelim
